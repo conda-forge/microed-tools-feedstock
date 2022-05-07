266b044ad9031c005817868c46059bb6884b53e3
Johan Hattne <johan@hattne.se>
2022-05-06 17:36:13 -0700

    Attempt to fix build * Force CMake to use sysroot's iconv on Linux and macOS * Need vc 14 to build with Python 3.5-3.10 on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 
+ls "${CONDA_BUILD_SYSROOT}"
+if test -n "${LD_RUN_PATH}"; then
+    echo "Linux conditinal"
+    iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
+    iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
+elif test -n "${OSX_ARCH}"; then
+    echo "macOS conditinal"
+    iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
+    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
+fi
+
 test "${PKG_BUILDNUM}" != "0" && buildmetadata="${PKG_BUILDNUM}"
 cat << EOF > "${SRC_DIR}/MICROED-TOOLS-VERSION-FILE"
 MICROED_TOOLS_VERSION_BRANCH=experimental/jiffies
@@ -13,7 +24,7 @@ PACKAGE_VERSION_PATCH=0
 PACKAGE_VERSION_TWEAK=0
 EOF
 
-cmake ${CMAKE_ARGS}                              \
+cmake ${CMAKE_ARGS} ${iconv_args}                \
     -DBUILD_PYTHON_MODULE:BOOL=ON                \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
