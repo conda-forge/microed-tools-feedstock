b5dcb1df7723eeb781bfbc4b0ca9902e05983c4f
Johan Hattne <johan@hattne.se>
2023-11-03 15:43:45 -0700

    From tvips-tools-feedstock: use text-based stub libraries with macOS SDK 10.11 and later

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
 iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
-if test -n "${OSX_ARCH}"; then
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.tbd"
-else
+if test -n "${LD_RUN_PATH}"; then
     iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
+elif test -n "${OSX_ARCH}"; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    major=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^\([0-9]*\).*/\1/"`
+    minor=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^[0-9]*\.\([0-9]*\).*/\1/"`
+    if test "${major}" -gt "10" -o \( "${major}" -eq "10" -a "${minor}" -ge "11" \); then
+        libiconv="libiconv.tbd"
+    else
+        libiconv="libiconv.dylib"
+    fi
+    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/${libiconv}"
 fi
 
 test "${PKG_BUILDNUM}" != "0" && sed                                       \
