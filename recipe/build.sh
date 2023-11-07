f3d38a34e1544edeed632416a530b0d53aee3341
Johan Hattne <johan@hattne.se>
2023-11-07 12:32:54 -0800

    Revert ${MACOSX_SDK_VERSION} to ${MACOSX_DEPLOYMENT_TARGET}

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,8 @@ if test -n "${LD_RUN_PATH}"; then
 elif test -n "${OSX_ARCH}"; then
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
     echo "Have '${MACOSX_SDK_VERSION}' and '${MACOSX_DEPLOYMENT_TARGET}' and '${CONDA_BUILD_SYSROOT}'"
-    major=`echo "${MACOSX_SDK_VERSION}" | sed -e "s/^\([0-9]*\).*/\1/"`
-    minor=`echo "${MACOSX_SDK_VERSION}" | sed -e "s/^[0-9]*\.\([0-9]*\).*/\1/"`
+    major=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^\([0-9]*\).*/\1/"`
+    minor=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^[0-9]*\.\([0-9]*\).*/\1/"`
     echo "Got major='${major}'"
     echo "Got minor='${minor}'"
     if test "${major}" -gt "10" -o \( "${major}" -eq "10" -a "${minor}" -ge "11" \); then
@@ -19,7 +19,8 @@ elif test -n "${OSX_ARCH}"; then
 fi
 
 test "${CONDA_BUILD_CROSS_COMPILATION}" = "1" &&               \
-    numpy_args="-DPython3_NumPy_INCLUDE_DIR:PATH=\"${SP_DIR}/numpy/core/include\""
+    numpy_args="-DPython3_NumPy_INCLUDE_DIR:PATH=${SP_DIR}/numpy/core/include"
+echo "HAVE '${CONDA_BUILD_CROSS_COMPILATION}' and '${numpy_args}'"
 
 test "${PKG_BUILDNUM}" != "0" && sed                                       \
     -e "s:^\(MICROED_TOOLS_VERSION_BUILDMETADATA=\).*$:\1${PKG_BUILDNUM}:" \
