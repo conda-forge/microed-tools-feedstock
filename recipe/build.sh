f118a89c848457478150496a861c1fd0cedcb819
Johan Hattne <johan@hattne.se>
2024-09-24 20:06:28 -0700

    Syntax error

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,20 +18,20 @@ iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
 if test -n "${LD_RUN_PATH}"; then
     iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
 elif test -n "${OSX_ARCH}"; then
-    if _version_lt("${MACOSX_DEPLOYMENT_TARGET}" "10.11"); then
+    if _version_lt "${MACOSX_DEPLOYMENT_TARGET}" "10.11" ; then
         _libiconv="${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
     else
         _libiconv="${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.tbd"
     fi
     iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${_libiconv}"
 
-    if _version_lt("${MACOSX_DEPLOYMENT_TARGET}" "10.14"); then
+    if _version_lt "${MACOSX_DEPLOYMENT_TARGET}" "10.14" ; then
         CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
     fi
 fi
 
 if test -n "${CONDA_BUILD_CROSS_COMPILATION}"; then
-    if _version_lt("${NPY_VER}" "2.0"); then
+    if _version_lt "${NPY_VER}" "2.0" ; then
         _include_dir="${SP_DIR}/numpy/core/include"
     else
         _include_dir="${SP_DIR}/numpy/_core/include"
